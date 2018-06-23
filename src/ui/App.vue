9e13d379532f247abc67a1969cca0854f551aeb1
Oliver Harrison <olivermharrison@gmail.com>
2018-06-23 17:09:24 +0100

    [+] Split up the app container TS, HTML & CSS across separate files

---
+++ src/ui/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <div class="io">
+      <div>
+        <canvas width="250" height="250  "></canvas>
+      </div>
+      <div>
+        <h1>imogen</h1>
+        <p>a colour distribution visualiser</p>
+      </div>
+      <div>
+        <canvas width="250" height="250  "></canvas>
+      </div>
+    </div>
+
+    <div class="actions">
+      <div><button>undo</button></div>
+      <div><button>add operation</button></div>
+      <div><button>reset</button></div>
+    </div>
+  </div>
+</template>
+
+<script src="./app.ts" lang="ts"></script>
+<style src="./app.scss" lang="scss"></style>
