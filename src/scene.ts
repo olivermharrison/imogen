fc04ef26cfbfcfdc703c6fef67ce04fe6d1f37a9
Oliver Harrison <olivermharrison@gmail.com>
2018-06-23 19:09:50 +0100

    [+] Scene light and update loop

--- src/scene.ts
+++ src/scene.ts
@@ -8,10 +8,22 @@ export default class Scene {
     constructor() {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.camera.position.y = 300;
 
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this.renderer.domElement );
+
+        // lights
+        const ambientLight = new THREE.AmbientLight( 0xffffff );
+        this.scene.add( ambientLight );
+
+        var animate = () => {
+            requestAnimationFrame( animate );
+            this.camera.lookAt( this.scene.position );
+            this.renderer.render(this.scene, this.camera);
+        };
+        animate();
     }
 }
 
