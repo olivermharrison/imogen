405ab44c1055ceaa24d50a26638fd4227a89dc64
Oliver Harrison <olivermharrison@gmail.com>
2018-06-23 18:10:41 +0100

    [+] Basic image rendering on a canvas

--- src/CanvasImage.ts
+++ src/CanvasImage.ts
@@ -1,7 +1,8 @@
 
 export default class CanvasImage {
 
-    element: HTMLCanvasElement;
+    element?: HTMLCanvasElement;
+    context?: CanvasRenderingContext2D;
 
     data?: any;
 
@@ -9,10 +10,18 @@ export default class CanvasImage {
         let el = document.getElementById(id) as HTMLCanvasElement;
         if (el) {
             this.element = el;
-        } else {
-            console.error('Canvas element not found');
-            this.element = document.createElement('canvas');
+            let c = this.element.getContext('2d');
+            if (c) {
+                this.context = c as CanvasRenderingContext2D;
+            }
         }
+    }
+
+    drawImage(image: HTMLImageElement) {
+        if (!this.context) return;
+        
+        this.context.drawImage(image, 0, 0);
+        this.data = this.context.getImageData(0, 0, image.width, image.height).data;
     }
 
     setData(newData: any) {
--- src/graph.ts
+++ src/graph.ts
@@ -4,22 +4,26 @@ import CanvasImage from './CanvasImage';
 export default class Graph {
 
     inputImage: HTMLImageElement;
-
-    inputCanvas?: CanvasImage;
-    outputCanvas?: CanvasImage;
+    inputCanvas: CanvasImage;
+    outputCanvas: CanvasImage;
 
     constructor() {
         this.inputImage = new Image();
+        this.inputCanvas = new CanvasImage('');
+        this.outputCanvas = new CanvasImage('');
     }
 
-    mounted() {
+    init() {
         this.inputCanvas = new CanvasImage('inputCanvas');
+        this.outputCanvas = new CanvasImage('outputCanvas');
+        this.setInputImage('./logo.png');
     }
 
     setInputImage(file: string) {
         this.inputImage.src = file;
-        this.inputImage.onload = function() {
-            console.log('image loaded.')
+        this.inputImage.onload = () => {
+            this.inputCanvas.drawImage(this.inputImage);
+            this.outputCanvas.drawImage(this.inputImage);
         }
     }
 
