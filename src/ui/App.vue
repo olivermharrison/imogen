848a772b66b1c93471b93523a41c2e123dc0c273
Oliver Harrison <olivermharrison@gmail.com>
2018-06-23 18:15:47 +0100

    [+] UI operations

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -2,21 +2,25 @@
 <div id="app">
     <div class="io">
         <div>
-            <canvas width="250" height="250  "></canvas>
+            <canvas id="inputCanvas" width="250" height="250  "></canvas>
         </div>
         <div>
             <h1>imogen</h1>
             <p>a colour distribution visualiser</p>
         </div>
         <div>
-            <canvas width="250" height="250  "></canvas>
+            <canvas id="outputCanvas" width="250" height="250  "></canvas>
         </div>
     </div>
 
     <div class="actions">
-        <div><button>undo</button></div>
-        <div><button>add operation</button></div>
-        <div><button>reset</button></div>
+        <div><button @click="applyOperation('undo')">undo</button></div>
+        <div><button @click="showOperations = !showOperations">add operation</button></div>
+        <div><button @click="applyOperation('reset')">reset</button></div>
+    </div>
+
+    <div v-show="showOperations">
+        OPERATIONS
     </div>
 </div>
 </template>
