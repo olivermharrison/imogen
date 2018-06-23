59824aaf427caf1a8621deba049fe9cbea6d9424
Oliver Harrison <olivermharrison@gmail.com>
2018-06-23 17:06:47 +0100

    [+] Basic UI layout & styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <div class="io">
+      <div>
+        <canvas width="250" height="250  "></canvas>
+      </div>
+      <div>
+        <h1>imogen</h1>
+        <p>a colour distribution visualiser</p>
+      </div>
+      <div>
+        <canvas width="250" height="250  "></canvas>
+      </div>
+    </div>
+
+    <div class="actions">
+      <div><button>undo</button></div>
+      <div><button>add operation</button></div>
+      <div><button>reset</button></div>
+    </div>
   </div>
 </template>
 
@@ -13,11 +31,62 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
+  text-align: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  padding-top: 1rem;
+  color: rgb(251, 80, 80);
+
+  .io {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0.5rem;
+
+    div {
+      border: 1px solid red;
+    }
+  }
+}
+
+* {
+  margin: 0;
+  box-sizing: border-box;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0;
+}
+
+.actions {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  padding: 2rem;
+  
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  button {
+    padding: 1rem 2rem;
+    border-radius: 0.4rem;
+    border: 1px solid rgb(67, 181, 226);
+    background: none;
+    color: rgb(255, 255, 255);
+    font-size: 1rem;
+    outline: none;
+    cursor: pointer;
+  }
 }
 </style>
