9e5c75edb3ee7c2dc084b590b6a3104bf2163c42
Oliver Harrison <olivermharrison@gmail.com>
2019-04-22 12:11:18 +0100

    [~] Canvas resizing

--- src/CanvasImage.ts
+++ src/CanvasImage.ts
@@ -6,6 +6,9 @@ export default class CanvasImage {
 
     public data?: Uint8ClampedArray;
 
+    private MAX_WIDTH: number = 250;
+    private MAX_HEIGHT: number = 250;
+
     constructor(id: string) {
         const el = document.getElementById(id) as HTMLCanvasElement;
         if (el) {
@@ -19,7 +22,12 @@ export default class CanvasImage {
 
     public drawImage(image: HTMLImageElement): Uint8ClampedArray | undefined {
         if (!this.context) { return undefined; }
-
+        if (image.width <= this.MAX_WIDTH) {
+            this.element!.width = image.width;
+        }
+        if (image.height <= this.MAX_HEIGHT) {
+            this.element!.height = image.height;
+        }
         this.context.drawImage(image, 0, 0);
         return this.context.getImageData(0, 0, image.width, image.height).data;
     }
