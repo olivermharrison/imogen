5e5552a7765ca1f519825ef46908e6aa665489d1
Oliver Harrison <olivermharrison@gmail.com>
2019-04-27 12:58:13 +0100

    [~] Image selector styling

--- src/ui/image-selector/image-selector.vue
+++ src/ui/image-selector/image-selector.vue
@@ -1,24 +1,17 @@
 <template>
-    <modal>
+    <modal v-on:close="close()">
         <div class="image-selector">
-            <div class="upload">
-                <label for="fileUploadId" id="fileUploadLabel">upload image</label>
-                <input id="fileUploadId" ref="fileUpload" type="file" accept="image/*" @change="uploadImage()"/>
-            </div>
- 
-            <br>
-            <hr>
             <div class="sample-image-container">
                 <div v-for="(image, index) in sampleImages" :key="index" @click="selectImage(image)">
                     <img :src="getPathToImage(image)" />
                 </div>
             </div>
-
-            <div>
-                <button @click="close()">cancel</button>
+            <div class="upload">
+                <label for="fileUploadId" id="fileUploadLabel" class="accent big">upload image</label>
+                <input id="fileUploadId" ref="fileUpload" type="file" accept="image/*" @change="uploadImage()"/>
+                <button @click="close(false)">close</button>
             </div>
         </div>
-
     </modal>
 </template>
 <script src="./image-selector.ts" lang="ts"></script>
