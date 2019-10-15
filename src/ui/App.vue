28833d38bb6e3b03e4db355abb652fb08a985aaa
Oliver Harrison <olivermharrison@gmail.com>
2019-10-15 18:19:19 +0100

    [+] Layout & styling update

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -1,24 +1,31 @@
 <template>
 <div id="app">
+    <h1>i m o g e n</h1>
     <div class="io" :style="{ filter: (showOperations || showImageSelector) ? 'blur(5px)' : 'none' }">
         <div>
+            <span style="text-align: left">input</span>
             <canvas id="inputCanvas" width="250" height="250  "></canvas>
         </div>
-        <div>
-            <h1>imogen</h1>
-            <p>a colour distribution visualiser</p>
+        <div class="io-center">
+            <button class="btn-select-desktop" @click="showImageSelector = true">select image</button> 
         </div>
         <div>
+            <span style="text-align: right">output</span>
             <canvas id="outputCanvas" width="250" height="250  "></canvas>
         </div>
     </div>
 
-    <div class="actions"  :style="{ filter: (showOperations || showImageSelector) ? 'blur(5px)' : 'none' }">
-        <div><button @click="applyOperation({title: 'undo'})">undo</button></div>
-        <div><button @click="showOperations = !showOperations" class="accent">add operation</button></div>
-        <div><button @click="applyOperation({title: 'reset'})">reset</button></div>
-    </div>
+    <button class="btn-select-mobile" @click="showImageSelector = true" >select image</button>
 
+    <div class="actions"  :style="{ filter: (showOperations || showImageSelector) ? 'blur(5px)' : 'none' }">
+        <div>
+            <button v-if="graph.updates.length > 1" @click="applyOperation({title: 'undo'})">undo</button>
+        </div>
+        <div><button @click="showOperations = !showOperations" class="accent big">add operation</button></div>
+        <div>
+            <button v-if="graph.updates.length > 1" @click="applyOperation({title: 'reset'})">reset</button>
+        </div>
+    </div>
 
 
     <transition name="fade">
